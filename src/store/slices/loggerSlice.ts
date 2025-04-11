8e8ca971512df745b15bb897ce6b0361875c323f
JungEun <foreverkiddd@gmail.com>
2025-04-11 15:15:45 +0900

    250411 BoardList, SideForm, List Container, List, Task, ActionButton, DropDownForm 생성

--- src/store/slices/loggerSlice.ts
+++ src/store/slices/loggerSlice.ts
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 import { ILogItem } from "../../types";
 
 
@@ -14,8 +14,11 @@ const loggerSlice = createSlice({
     name: 'logger',
     initialState,
     reducers: {
-
+        addLog: (state, {payload}: PayloadAction<ILogItem>) => {
+            state.logArray.push(payload);
+        }
     }
 })
 
+export const { addLog } = loggerSlice.actions;
 export const loggerReducer = loggerSlice.reducer;
