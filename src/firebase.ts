d07a0287a3b75970edd9c216860b8f34b1d58aa4
JungEun <foreverkiddd@gmail.com>
2025-04-15 12:30:02 +0900

    250415 Drag and Drop 기능, Drop End 기능, Firebase 연결, 로그인, 로그아웃 기능

---
+++ src/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyA594aD-f5uP4ydf9pslzQuYuMcSrIUaLU",
+  authDomain: "react-test-app-2-3585d.firebaseapp.com",
+  projectId: "react-test-app-2-3585d",
+  storageBucket: "react-test-app-2-3585d.firebasestorage.app",
+  messagingSenderId: "109825215425",
+  appId: "1:109825215425:web:cecab32fb9f0cdc16cbd40"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
--- src/store/slices/boardSlice.ts
+++ src/store/slices/boardSlice.ts
@@ -36,6 +36,15 @@ type TDeleteBoardAction = {
     boardId: string;
 }
 
+type TSortAction = {
+    boardIndex: number;
+    droppableIdStart: string;
+    droppableIdEnd: string;
+    droppableIndexStart: number;
+    droppableIndexEnd: number;
+    draggableId: string;
+}
+
 const initialState: TBoardsState = {
     modalActive: false,
     boardArray: [
@@ -184,10 +193,35 @@ const boardsSlice = createSlice({
 
         setModalActive: (state, {payload}: PayloadAction<boolean>) => {
             state.modalActive = payload
+        },
+
+        sort: (state, {payload}:PayloadAction<TSortAction>) => {
+            // same list
+            if(payload.droppableIdStart === payload.droppableIdEnd) {
+                const list = state.boardArray[payload.boardIndex].lists.find(
+                    list => list.listId === payload.droppableIdStart
+                )
+                // 변경시키는 아이템을 배열에서 지워줌
+                // return 값으로 지워진 아이템을 잡아줌
+                const card = list?.tasks.splice(payload.droppableIndexStart, 1);
+                list?.tasks.splice(payload.droppableIndexEnd, 0, ...card!);
+            }
+
+            if(payload.droppableIdStart !== payload.droppableIdEnd) {
+                const listStart = state.boardArray[payload.boardIndex].lists.find(
+                    list => list.listId === payload.droppableIdStart
+                )
+
+                const card = listStart!.tasks.splice(payload.droppableIndexStart, 1);
+                const listEnd = state.boardArray[payload.boardIndex].lists.find(
+                    list => list.listId === payload.droppableIdEnd
+                )
+                listEnd?.tasks.splice(payload.droppableIndexEnd, 0, ...card);
+            }
         }
     }
 })
 
 
-export const { addBoard, deleteBoard, deleteList, setModalActive, addList, addTask, updateTask, deleteTask } = boardsSlice.actions;
+export const { sort, addBoard, deleteBoard, deleteList, setModalActive, addList, addTask, updateTask, deleteTask } = boardsSlice.actions;
 export const boardsReducer = boardsSlice.reducer;
